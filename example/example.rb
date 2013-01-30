6a95f5cf28aabab58211295a4ee8e4268ea5f35f
Joel Plane <joel.plane@gmail.com>
2013-01-30 22:08:18 +1100

    enable REE copy-on-write GC in example

--- example/example.rb
+++ example/example.rb
@@ -1,5 +1,8 @@
 require File.expand_path "../lib/parallel_work", File.dirname(__FILE__)
 
+# Turn on REE copy-on-write GC if we're running on REE
+GC.copy_on_write_friendly = true if GC.respond_to? :copy_on_write_friendly?
+
 work = (1..400).to_a
 
 ParallelWork.process work, 8 do |data|
